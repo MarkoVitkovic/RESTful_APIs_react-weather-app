5745a2fa4f068cf4a8b41a1fa126c6df778986e3
Marko Vitković <mavitkov@vsite.hr>
2020-03-22 03:00:03 +0100

    secured

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends React.Component{
     const country = e.target.elements.country.value;
 
     if(city && country){
-      const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`)
+      const api_call= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`)
       const res = await api_call.json();
       console.log(res);
       this.setState({
